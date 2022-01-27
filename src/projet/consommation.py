de13034375eda666a450bae20ede7947e4142c24
ColineVL <coline.vanleeuwen@gmail.com>
2022-01-27 18:51:04 +0100

    producteurs : on distingue les différents groupes

--- src/projet/consommation.py
+++ src/projet/consommation.py
@@ -2,3 +2,5 @@
 
 consoNord = 217
 consoSud = 265
+
+consoTotale = consoNord + consoSud
--- src/projet/producteurs.py
+++ src/projet/producteurs.py
@@ -1,30 +1,43 @@
 class Producteur:
-    def __init__(self, zone, centrale, type, capaciteMax):
+    def __init__(self, zone, centrale, type, puissanceMax, puissanceMin):
         self.type = type  # charbon, TAC, diesel, renouvelable
-        self.centrale = centrale  # Bois Rouge, Le Gol, hydro, solaire...
+        self.nomCentrale = centrale  # Bois Rouge, Le Gol, hydro, solaire...
         self.zone = zone  # Nord ou Sud
-        self.capaciteMax = capaciteMax  # en MW
+        self.puissanceMax = puissanceMax  # en MW
+        self.puissanceMin = puissanceMin  # en MW
+        self.coutMarginal = 0  # en € / MWh
 
     def myfunc(self):
         print("Je m'appelle " + self.centrale)
 
 
+# Tableau regroupant tous les producteurs
 tousProducteurs = [
-    # Thermiques
-    Producteur("Sud", "Bois Rouge", "charbon", 100),
-    Producteur("Nord", "Le Gol", "charbon", 110),
-    Producteur("Nord", "La Baie", "tac", 80),
-    Producteur("Nord", "Le Port Est", "diesel", 216),
     # Renouvelables
-    Producteur("Sud", "Hydraulique", "renouvelable", 134),
-    Producteur("Sud", "Solaire Sud", "renouvelable", 115),
-    Producteur("Sud", "Bioénergies", "renouvelable", 4),
-    Producteur("Sud", "Eolien", "renouvelable", 16),
-    Producteur("Nord", "Solaire Nord", "renouvelable", 58),
+    Producteur("Sud", "Hydraulique", "renouvelable", 134, 0),
+    Producteur("Sud", "Solaire_Sud", "renouvelable", 115, 0),
+    Producteur("Sud", "Bioenergies", "renouvelable", 4, 0),
+    Producteur("Sud", "Eolien", "renouvelable", 16, 0),
+    Producteur("Nord", "Solaire_Nord", "renouvelable", 58, 0),
 ]
 
-# Cout marginal, en € par MWh
-coutMarginal = {"tac": 150, "diesel": 80, "charbon": 40, "renouvelable": 0}
+# On ajoute les thermiques : dans chaque site de production, il y a plusieurs groupes qui peuvent être activés indépendamment les uns des autres.
+# Dans le range, le 1+3 signifie qu'il y a 3 groupes dans ce site de production
+for i in range(1, 1 + 3):
+    tousProducteurs.append(Producteur("Sud", f"Bois_Rouge_{i}", "charbon", 33, 10))
+for i in range(1, 1 + 3):
+    tousProducteurs.append(Producteur("Nord", f"Le_Gol_{i}", "charbon", 37, 10))
+for i in range(1, 1 + 3):
+    tousProducteurs.append(Producteur("Nord", f"La_Baie_{i}", "tac", 40, 15))
+for i in range(1, 1 + 3):
+    tousProducteurs.append(Producteur("Nord", f"Le_Port_Est_{i}", "diesel", 18, 0))
 
-capaTotale = sum([prod.capaciteMax for prod in tousProducteurs])
-print(capaTotale)
+# Cout marginal, en € par MWh
+dataCoutMarginal = {"tac": 150, "diesel": 80, "charbon": 40, "renouvelable": 0}
+
+# On calcule le cout marginal de chaque site de production
+for prod in tousProducteurs:
+    prod.coutMarginal = dataCoutMarginal[prod.type]
+
+# Juste en rappel
+capaTotale = sum([prod.puissanceMax for prod in tousProducteurs])
