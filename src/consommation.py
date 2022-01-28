26e0e903dd2ebd55456517dd227295f7e1f6f7fc
ColineVL <coline.vanleeuwen@gmail.com>
2022-01-28 11:31:21 +0100

    donnees conso juste pour test

--- src/consommation.py
+++ src/consommation.py
@@ -1,6 +1,11 @@
+import numpy as np
+
 """ Consommation en MW """
 
 consoNord = 217
 consoSud = 265
 
 consoTotale = consoNord + consoSud
+
+demand = np.loadtxt("./src/demand.txt")
+consoJournee = demand[:24]
