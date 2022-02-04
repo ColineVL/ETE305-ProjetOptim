30bb2c51bfcba7f8696a88167e5fc82ddb1f27a6
ColineVL <coline.vanleeuwen@gmail.com>
2022-02-04 14:10:24 +0100

    on peut choisir le nb d'heures

--- src/readExcel.py
+++ src/readExcel.py
@@ -4,18 +4,19 @@ wb = load_workbook("./data/donnees optim sujet interco.xlsx")
 sheetNord = wb[wb.sheetnames[1]]
 sheetSud = wb[wb.sheetnames[2]]
 
-# nbHeures = len(sheetNord["B"])
-nbHeures = 24 * 30  # 1 mois
+nbHeures = len(sheetNord["B"])
+# nbHeures = 24 * 30  # 1 mois
 
-consoNord = [float(cell.value) for cell in sheetNord["B"][1 : nbHeures + 1]]
-prodSolaireNord = [float(cell.value) for cell in sheetNord["C"][1 : nbHeures + 1]]
+consoNord = [float(cell.value) for cell in sheetNord["B"][1:nbHeures]]
+prodSolaireNord = [float(cell.value) for cell in sheetNord["C"][1:nbHeures]]
 
-consoSud = [float(cell.value) for cell in sheetSud["B"][1 : nbHeures + 1]]
-prodSolaireSud = [float(cell.value) for cell in sheetSud["C"][1 : nbHeures + 1]]
-prodEolienSud = [float(cell.value) for cell in sheetSud["D"][1 : nbHeures + 1]]
-prodHydroSud = [float(cell.value) for cell in sheetSud["E"][1 : nbHeures + 1]]
-prodBioenergiesSud = [float(cell.value) for cell in sheetSud["F"][1 : nbHeures + 1]]
+consoSud = [float(cell.value) for cell in sheetSud["B"][1:nbHeures]]
+prodSolaireSud = [float(cell.value) for cell in sheetSud["C"][1:nbHeures]]
+prodEolienSud = [float(cell.value) for cell in sheetSud["D"][1:nbHeures]]
+prodHydroSud = [float(cell.value) for cell in sheetSud["E"][1:nbHeures]]
+prodBioenergiesSud = [float(cell.value) for cell in sheetSud["F"][1:nbHeures]]
 
+nbHeures = len(consoNord)
 
 assert (
     len(consoNord)
