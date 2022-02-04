c1e3b33217955b01f3a443f1abf66eacd23ed58b
ColineVL <coline.vanleeuwen@gmail.com>
2022-02-04 18:24:38 +0100

    pas de production < 0

--- src/readExcel.py
+++ src/readExcel.py
@@ -7,14 +7,21 @@ sheetSud = wb[wb.sheetnames[2]]
 # nbHeures = len(sheetNord["B"])
 nbHeures = 24 * 30  # 1 mois
 
-consoNord = [float(cell.value) for cell in sheetNord["B"][1:nbHeures]]
-prodSolaireNord = [float(cell.value) for cell in sheetNord["C"][1:nbHeures]]
 
-consoSud = [float(cell.value) for cell in sheetSud["B"][1:nbHeures]]
-prodSolaireSud = [float(cell.value) for cell in sheetSud["C"][1:nbHeures]]
-prodEolienSud = [float(cell.value) for cell in sheetSud["D"][1:nbHeures]]
-prodHydroSud = [float(cell.value) for cell in sheetSud["E"][1:nbHeures]]
-prodBioenergiesSud = [float(cell.value) for cell in sheetSud["F"][1:nbHeures]]
+def cleanValue(value):
+    if value < 0:
+        return 0
+    return float(value)
+
+
+consoNord = [cleanValue(cell.value) for cell in sheetNord["B"][1:nbHeures]]
+prodSolaireNord = [cleanValue(cell.value) for cell in sheetNord["C"][1:nbHeures]]
+
+consoSud = [cleanValue(cell.value) for cell in sheetSud["B"][1:nbHeures]]
+prodSolaireSud = [cleanValue(cell.value) for cell in sheetSud["C"][1:nbHeures]]
+prodEolienSud = [cleanValue(cell.value) for cell in sheetSud["D"][1:nbHeures]]
+prodHydroSud = [cleanValue(cell.value) for cell in sheetSud["E"][1:nbHeures]]
+prodBioenergiesSud = [cleanValue(cell.value) for cell in sheetSud["F"][1:nbHeures]]
 
 nbHeures = len(consoNord)
 
