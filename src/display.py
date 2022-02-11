d88444a4da00db8608ac77f7555cf476e29bbe39
Shiwen SUN <Shiwen.SUN@student.isae-supaero.fr>
2022-02-11 17:03:24 +0100

    displayv1

--- src/display.py
+++ src/display.py
@@ -11,38 +11,82 @@ def affichageResultats(mesZones, nbHeures):
     #     plt.legend()
          
     for zone in mesZones.values():
-        plt.figure(f"Tous les producteurs dispatchables {zone.nom}")
+        # Energie renouvelable
+        # on a deja zone.productionFatal
 
-        # Demande
-        plt.plot(zone.conso, "b", label=f"Demande {zone.nom}")
-
-        # # Energie renouvelable
-        # RenewSum = [0.]
-        # for h in range(nbHeures):
-        #     RenewSum.append(sum(prod.production[h] for prod in zone.producteursFatal))
-        # plt.plot(zone.productionFatal)
-
-
-        # Producteur Dispatchable + Interconnexion
-        ProdSum = [0.] * nbHeures
-        for prod in zone.producteursDispatchable:
-            for h in range(nbHeures):
-                ProdSum[h] += prod.solutionProduction[h] + zone.solutionIntercoVersMoi[h] - mesZones["Sud" if zone.nom == "Nord" else "Nord"].solutionIntercoVersMoi[h] +zone.productionFatal[h]
-            plt.plot(ProdSum,label=prod.nomCentrale)
-            # plt.fill(TotalSum,label=prod.nomCentrale)
-            # plt.fill_between(ProdSum,interpolate=True)
-        plt.legend()
-
-
-    # On compare la demande et la production, sur les deux zones en même temps
-    plt.figure("Comparer demande et production")
-    for zone in mesZones.values():
-        plt.plot(zone.conso, label=f"Demande {zone.nom}")
-        prodTotale = [
-            sum(prod.solutionProduction[h] for prod in zone.producteursDispatchable)
+        # Interco
+        IntercoZone = [
+            zone.solutionIntercoVersMoi[h] - mesZones["Sud" if zone.nom == "Nord" else "Nord"].solutionIntercoVersMoi[h]
             for h in range(nbHeures)
         ]
-        plt.plot(prodTotale, label=f"Production totale {zone.nom}")
-        plt.legend()
+        
+        # Charbon
+        charbon = [
+            sum(
+                prod.solutionProduction[h]
+                for prod in zone.producteursDispatchable
+                if prod.type == 'charbon'
+            )
+            for h in range(nbHeures)
+        ]
+        
+        # Diesel
+        diesel = [
+            sum(
+                prod.solutionProduction[h]
+                for prod in zone.producteursDispatchable
+                if prod.type == 'diesel'
+            )
+            for h in range(nbHeures)
+        ]
+    
+        # TAC
+        tac = [
+            sum(
+                prod.solutionProduction[h]
+                for prod in zone.producteursDispatchable
+                if prod.type == 'tac'
+            )
+            for h in range(nbHeures)
+        ]
+       
+
+
+        # # Producteur Dispatchable + Interconnexion
+        # ProdSum = [0.] * nbHeures
+        # SolutionInterco = [0.] * nbHeures
+        # for prod in zone.producteursDispatchable:
+        #     for h in range(nbHeures):
+        #         SolutionInterco[h] =  zone.solutionIntercoVersMoi[h] - mesZones["Sud" if zone.nom == "Nord" else "Nord"].solutionIntercoVersMoi[h]
+        #         ProdSum[h] += prod.solutionProduction[h] + SolutionInterco[h] +zone.productionFatal[h]
+        #     plt.plot(ProdSum,label=prod.nomCentrale)
+        #     # plt.fill(TotalSum,label=prod.nomCentrale)
+        #     # plt.fill_between(ProdSum,interpolate=True)
+        # plt.legend()
+
+
+    # # On compare la demande et la production, sur les deux zones en même temps
+    # plt.figure("Comparer demande et production")
+    # for zone in mesZones.values():
+    #     plt.plot(zone.conso, label=f"Demande {zone.nom}")
+    #     prodTotale = [
+    #         sum(prod.solutionProduction[h] for prod in zone.producteursDispatchable)
+    #         for h in range(nbHeures)
+    #     ]
+    #     plt.plot(prodTotale, label=f"Production totale {zone.nom}")
+    #     plt.legend()
+
+
+    plt.figure(f"Tous les producteurs dispatchables {zone.nom}")
+
+    # Demande
+    plt.plot(zone.conso, "b", label=f"Demande {zone.nom}")
+    plt.plot(IntercoZone,label =f"IntercoZone")
+    Niveau1 =[
+        IntercoZone[h] + zone.productionFatal[h]
+        for h in range(nbHeures)
+    ]
+    plt.fill_between(range(nbHeures),IntercoZone,Niveau1,color = 'red',label =f"Production Fatal")
+
 
     plt.show()
