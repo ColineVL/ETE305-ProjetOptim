b60ca6a372261bf004b0f5cee14c2c5edc2b5e21
ColineVL <coline.vanleeuwen@gmail.com>
2022-02-20 18:50:50 +0100

    toutes data enregistrées

--- src/postTraitementFiles/writeExcel.py
+++ src/postTraitementFiles/writeExcel.py
@@ -1,33 +1,47 @@
 from openpyxl import Workbook
 from datetime import datetime
 
-from enums import ZoneName
+from readExcel import dates
 
 
 def writeResultsInExcel(mesZones, nbHeures):
     wb = Workbook()
-    timestamp = datetime.now().strftime("%d%m%Y-%H%M%S")
-    dest_filename = f"src/postTraitementFiles/results_{timestamp}.xlsx"
 
+    """ On note l'interconnexion """
     ws1 = wb.active
-    zone = mesZones[ZoneName.NORD]
-    ws1.title = f"{zone.nom.name}_dispatchable"
+    ws1.title = "Interconnexion"
 
+    ws1.append(["Dates"] + [f"vers {zone.nom.name}" for zone in mesZones.values()])
     for h in range(nbHeures):
         ws1.append(
-            [prod.solutionProduction[h] for prod in zone.producteursDispatchable]
+            [dates[h]] + [zone.solutionIntercoVersMoi[h] for zone in mesZones.values()]
         )
 
-    # for row in range(1, 40):
-    #     ws1.append(range(600))
+    for zone in mesZones.values():
+        """On note le dispatchable"""
+        ws = wb.create_sheet(title=f"{zone.nom.name}_dispatchable")
+        # Noms des centrales
+        ws.append(
+            ["Dates"] + [prod.nomCentrale for prod in zone.producteursDispatchable]
+        )
+        ws.append([""] + [prod.type.name for prod in zone.producteursDispatchable])
+        # Données solutions
+        for h in range(nbHeures):
+            ws.append(
+                [dates[h]]
+                + [prod.solutionProduction[h] for prod in zone.producteursDispatchable]
+            )
 
-    # ws2 = wb.create_sheet(title="Pi")
-    # ws2["F5"] = 3.14
-    # ws3 = wb.create_sheet(title="Data")
-    # for row in range(10, 20):
-    #     for col in range(27, 54):
-    #         _ = ws3.cell(
-    #             column=col, row=row, value="{0}".format(get_column_letter(col))
-    #         )
-    # print(ws3["AA10"].value)
+        """On note le fatal"""
+        ws = wb.create_sheet(title=f"{zone.nom.name}_fatal")
+        # Noms des centrales
+        ws.append(["Dates"] + [prod.nomCentrale for prod in zone.producteursFatal])
+        # Données solutions
+        for h in range(nbHeures):
+            ws.append(
+                [dates[h]] + [prod.production[h] for prod in zone.producteursFatal]
+            )
+
+    timestamp = datetime.now().strftime("%d%m%Y-%H%M%S")
+    dest_filename = f"src/postTraitementFiles/results_{timestamp}.xlsx"
     wb.save(filename=dest_filename)
