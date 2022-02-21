28f1660ac90ec59ffaa97ed794c53aefa1c154bb
ColineVL <coline.vanleeuwen@gmail.com>
2022-02-21 16:00:37 +0100

    amelioration : fatal / disp séparé

--- src/modelisation/ameliorations.py
+++ src/modelisation/ameliorations.py
@@ -13,33 +13,73 @@ class Amelioration:
         Nom de la zone concernée, NORD ou SUD
     coutInvestissement : float
         Cout d'investissement, en € par MW
+    """
+
+    def __init__(self, zone, type, coutInvestissement):
+        self.zone = zone
+        self.type = type
+        self.coutInvestissement = coutInvestissement
+
+    def __repr__(self):
+        return f"{self.zone} {self.type}"
+
+    def __str__(self):
+        return f"{self.zone} {self.type}"
+
+
+class AmeliorationFatal(Amelioration):
+    """
+    Représente une amélioration possible, pour un type d'énergie fatal et une zone
+
+    Attributes
+    ----------
+    type : TypeEnergie
+        Type d'énergie : HYDRO ou EOLIEN par exemple
+    zone : ZoneName
+        Nom de la zone concernée, NORD ou SUD
+    coutInvestissement : float
+        Cout d'investissement, en € par MW
     borneMax : float
         Valeur maximale de la capacité
     capaciteInitiale : float
        Valeur initiale de la capacité
+    capacite : pulp.LpVariable
+        On peut améliorer la capacité du producteur : nouvelle capa en MW
     """
 
-    def __init__(self, zone, type, coutInvestissement, borneMax, capaciteInitiale=0):
-        self.zone = zone
-        self.type = type
-        self.coutInvestissement = coutInvestissement
+    def __init__(self, zone, type, coutInvestissement, borneMax, capaciteInitiale):
+        super().__init__(zone, type, coutInvestissement)
         self.borneMax = borneMax
         self.capaciteInitiale = capaciteInitiale
 
-    def __repr__(self):
-        return f"{self.zone} {self.type} {self.borneMax}"
 
-    def __str__(self):
-        return f"{self.zone} {self.type} {self.borneMax}"
+class AmeliorationDispatchable(Amelioration):
+    """
+    Représente une amélioration possible, pour un type d'énergie dispatchable et une zone
+
+    Attributes
+    ----------
+    type : TypeEnergie
+        Type d'énergie : HYDRO ou EOLIEN par exemple
+    zone : ZoneName
+        Nom de la zone concernée, NORD ou SUD
+    coutInvestissement : float
+        Cout d'investissement, en € par MW
+    centraleConstruite : pulp.LpVariable
+        True si la centrale a été construite
+    """
+
+    def __init__(self, zone, type, coutInvestissement):
+        super().__init__(zone, type, coutInvestissement)
 
 
 ameliorations = [
-    Amelioration(ZoneName.NORD, TypeEnergie.SOLAIRE, 4.8415086 * 10**6, 153, 58),
-    Amelioration(ZoneName.SUD, TypeEnergie.SOLAIRE, 4.8415086 * 10**6, 305, 115),
-    Amelioration(ZoneName.SUD, TypeEnergie.EOLIEN, 2.26941 * 10**6, 42, 16),
+    AmeliorationFatal(ZoneName.NORD, TypeEnergie.SOLAIRE, 4.8415086 * 10**6, 153, 58),
+    AmeliorationFatal(ZoneName.SUD, TypeEnergie.SOLAIRE, 4.8415086 * 10**6, 305, 115),
+    AmeliorationFatal(ZoneName.SUD, TypeEnergie.EOLIEN, 2.26941 * 10**6, 42, 16),
     # Et maintenant les dispatchables
-    Amelioration(ZoneName.NORD, TypeEnergie.CHARBON, 2.2734171 * 10**6, 0, 0),
-    Amelioration(ZoneName.NORD, TypeEnergie.TAC, 1.169256 * 10**6, 0, 0),
-    Amelioration(ZoneName.SUD, TypeEnergie.CHARBON, 2.2734171 * 10**6, 0, 0),
-    Amelioration(ZoneName.SUD, TypeEnergie.TAC, 1.169256 * 10**6, 0, 0),
+    AmeliorationDispatchable(ZoneName.NORD, TypeEnergie.CHARBON, 2.2734171 * 10**6),
+    AmeliorationDispatchable(ZoneName.NORD, TypeEnergie.TAC, 1.169256 * 10**6),
+    AmeliorationDispatchable(ZoneName.SUD, TypeEnergie.CHARBON, 2.2734171 * 10**6),
+    AmeliorationDispatchable(ZoneName.SUD, TypeEnergie.TAC, 1.169256 * 10**6),
 ]
