f2e6616adabec7b47ea081b66afaa53241b33cc6
ColineVL <coline.vanleeuwen@gmail.com>
2022-03-16 19:00:11 +0100

    scénarios : mai, décembre

--- src/modelisation/readExcel.py
+++ src/modelisation/readExcel.py
@@ -5,11 +5,29 @@ wb = load_workbook("./data/donnees optim sujet interco.xlsx", data_only=True)
 sheetNord = wb[wb.sheetnames[1]]
 sheetSud = wb[wb.sheetnames[2]]
 
+# ICI le scénario qu'on veut tester, à modifier !
+scenario = 0
+# Scénario 0 : n premières heures
+# Scénario 1 : mois de mai
+# Scénario 2 : mois de décembre
 
-nbHeures = len(sheetNord["B"])
-# nbHeures = 24 * 30  # 1 mois
-# nbHeures = 24 * 65
-# FIXME à modifier pendant les tests
+if scenario == 0:
+    # modifier le nombre d'heures à prendre si on veut tester les n premières heures
+    nombreHeuresATester = len(sheetNord["B"])  # toute l'année
+    nombreHeuresATester = 24 * 30  # 1 mois
+
+    debut = 1
+    fin = debut + nombreHeuresATester - 1
+
+if scenario == 1:
+    # MOIS DE MAI
+    debut = 2877
+    fin = 3617
+
+if scenario == 2:
+    # MOIS DE DECEMBRE
+    debut = 8013
+    fin = 8760
 
 
 def cleanValue(value):
@@ -18,16 +36,16 @@ def cleanValue(value):
     return float(value)
 
 
-dates = [cell.value for cell in sheetNord["A"][1:nbHeures]]
+dates = [cell.value for cell in sheetNord["A"][debut:fin]]
 
-consoNord = np.array([cleanValue(cell.value) for cell in sheetNord["B"][1:nbHeures]])
-prodSolaireNord = [cleanValue(cell.value) for cell in sheetNord["C"][1:nbHeures]]
+consoNord = np.array([cleanValue(cell.value) for cell in sheetNord["B"][debut:fin]])
+prodSolaireNord = [cleanValue(cell.value) for cell in sheetNord["C"][debut:fin]]
 
-consoSud = np.array([cleanValue(cell.value) for cell in sheetSud["B"][1:nbHeures]])
-prodSolaireSud = [cleanValue(cell.value) for cell in sheetSud["C"][1:nbHeures]]
-prodEolienSud = [cleanValue(cell.value) for cell in sheetSud["D"][1:nbHeures]]
-prodHydroSud = [cleanValue(cell.value) for cell in sheetSud["E"][1:nbHeures]]
-prodBioenergiesSud = [cleanValue(cell.value) for cell in sheetSud["F"][1:nbHeures]]
+consoSud = np.array([cleanValue(cell.value) for cell in sheetSud["B"][debut:fin]])
+prodSolaireSud = [cleanValue(cell.value) for cell in sheetSud["C"][debut:fin]]
+prodEolienSud = [cleanValue(cell.value) for cell in sheetSud["D"][debut:fin]]
+prodHydroSud = [cleanValue(cell.value) for cell in sheetSud["E"][debut:fin]]
+prodBioenergiesSud = [cleanValue(cell.value) for cell in sheetSud["F"][debut:fin]]
 
 nbHeures = len(consoNord)
 
